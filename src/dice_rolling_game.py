c0197f5340bf487fef843637b0bb22168f138065
amisha065 <amishabanerjee065@gmail.com>
2024-10-06 22:33:23 -0700

    dice rolling game

---
+++ src/dice_rolling_game.py
@@ -0,0 +1,29 @@
+import random
+import sys
+
+def dice_roll(iterations):
+
+    result = ""
+    for i in range(iterations):
+         result += f"{random.randint(1, 6)} "
+    print(f"{result}")
+
+def play(choice):
+    match choice:
+            case 'y':
+                iterations = int(input("How many times? "))
+                dice_roll(iterations)
+                return True
+            case 'n':
+                print("Thanks for playing!")
+                return False
+            case _:
+                print("Invalid choice!")
+                return True
+
+if __name__ == "__main__":
+
+    active=True
+    while(active):
+        choice = input("Roll the dice? (y/n): ").lower()
+        active=play(choice)
