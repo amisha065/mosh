8b347b58bbce1addea76c9d295c450e207ff4cb5
amisha065 <amishabanerjee065@gmail.com>
2024-10-08 12:47:00 -0700

    quiz_game v2
    - List of dictionaries approach. We can think about
    dictionary of lists approach also.. but thats a bit complex
    - Simple checking logic. can be enhanced

--- src/quiz_game.py
+++ src/quiz_game.py
@@ -1,40 +1,70 @@
+'''
+Notes:
+======================================================================
+1) We learnt how to manage our database (question bank) using "list of dictionares approach" to in this assignment
+2) We saw a how to randomize the questions we ask using random.shuffle() function
+3) We saw a simple use of lambda fuction
+4) We used cprint to color our text on screen
+
+We can work on the following improvements:
+======================================================================
+Right now the order of our choices are hardcoded. Ex. 'B. Jupiter'. 
+The reason is to simplify comparison of the expected response  with the selected response.
+For example: user selects B.. and expected is B.. so the test is super simple..
+
+Ideally, we need the following thing.. 
+
+user selects A --> we should have a mechanism to map A to the displayed choice 'Jupiter' or whatver
+And the expected response will also be the string.. 
+and then we will compare the selected string to the expected syting
+
+'''
+
+
 # python3 -m pip install --upgrade termcolor
 import sys
+import random
 
 from termcolor import cprint
 
+# Helper lambdas to control the print colors
 print_red = lambda x: cprint(x, "red")
 print_green = lambda x: cprint(x, "green")
 
-counter = 0
+# APPROACH 1: List of dictionary approach
+question_bank = [
+       {'question': 'What is the capital of France?', 'options':['A. Paris','B. London','C. Rome'], 'correct': 'A'},
+       {'question': 'What is the largest planet in our solar system?', 'options':['A. Earth', 'B. Jupiter', 'C. Mars'], 'correct': 'B'},
+       {'question': 'What is the capital of France (fewer choices?', 'options':['A. Paris','B. London'], 'correct': 'A'},
+]
 
-print('Question 1: What is the capital of France?')
-print('a) Paris')
-print('b) London')
-print('c) Rome')
+def ask_question_give_options(question_number, question_answer):
+    print(f'Question {question_number+1}: {question_answer['question']}')
+    for option in question_answer['options']:
+            print(f'{option}')
 
-user_input = input('Your answer: ').upper()
-expected_answer = 'A'
 
-if user_input == expected_answer:
-    print_green("Correct!")
-    counter += 1
-else:
-    print_red(f'Wrong! The correct answer is {expected_answer}')
 
-print('Question 2: What is the capital of France?')
-print('a) Paris')
-print('b) London')
-print('c) Rome')
+def play_game():
 
-user_input = input('Your answer: ').upper()
-expected_answer = 'A'
+    # This will randomly shuffle the entries of question_bank. So, now when we iterate thru it they will be in random order
+    random.shuffle(question_bank)
 
-if user_input == expected_answer:
-    print_green("Correct!")
-    counter += 1
-else:
-    print_red(f'Wrong! The correct answer is {expected_answer}')
+    score = 0
+    for question_number, question_answer in enumerate(question_bank):
+        ask_question_give_options(question_number, question_answer)
+        
+        selected_answer = input('Your answer: ').upper()
+        expected_answer = question_answer['correct']
 
-print(f'Quiz over! Your final score is {counter} out of 2')
+        if selected_answer == expected_answer:
+            print_green("Correct!\n")
+            score += 1
+        else:
+            print_red(f'Wrong! The correct answer is {expected_answer}\n')
 
+    print(f'Quiz over! Your final score is {score} out of {len(question_bank)}')
+
+
+if __name__ == "__main__":
+    play_game()
